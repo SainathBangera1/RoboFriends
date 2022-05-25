20fb0b0c735183d7afc3eece3481d84e16967b4e
SainathBangera1 <psainathbangera@gmail.com>
2022-05-25 17:33:38 +0530

    Updates

---
+++ precache-manifest.a666d4570f2468f546945ffb8d5ab2ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea978e8eb990671b7de9",
+    "url": "/robofriends/static/css/main.48c33bc2.chunk.css"
+  },
+  {
+    "revision": "ea978e8eb990671b7de9",
+    "url": "/robofriends/static/js/main.ea978e8e.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "4ee2d1bb4fc60408282b",
+    "url": "/robofriends/static/css/2.027689f5.chunk.css"
+  },
+  {
+    "revision": "4ee2d1bb4fc60408282b",
+    "url": "/robofriends/static/js/2.4ee2d1bb.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "73ddaa250e1f2bf82806775f5a54ca26",
+    "url": "/robofriends/index.html"
+  }
+];
